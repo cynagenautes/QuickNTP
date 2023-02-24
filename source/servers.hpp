3c3f9b3bb3392f426e2d8e6f2e4d307b41d06d50
cynagenautes <38454938+cynagenautes@users.noreply.github.com>
2023-02-24 21:28:32 +0000

    Optimize servers list for use in Japan

--- source/servers.hpp
+++ source/servers.hpp
@@ -1,14 +1,8 @@
 #include "vector-pair.hpp"
 
 vector_pair NTPSERVERS = {
-    {"NTP Pool - Main", "pool.ntp.org"},
+    {"NICT", "ntp.nict.jp"},
+    {"INTERNET MULTIFEED", "ntp.jst.mfeed.ad.jp"},
     {"Cloudflare", "time.cloudflare.com"},
     {"Google", "time.google.com"},
-    {"NIST - Main", "time.nist.gov"},
-    {"NTP Pool - Africa", "africa.pool.ntp.org"},
-    {"NTP Pool - Asia", "asia.pool.ntp.org"},
-    {"NTP Pool - Europe", "europe.pool.ntp.org"},
-    {"NTP Pool - North America", "north-america.pool.ntp.org"},
-    {"NTP Pool - Oceania", "oceania.pool.ntp.org"},
-    {"NTP Pool - South America", "south-america.pool.ntp.org"},
 };
